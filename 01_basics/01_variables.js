44b71eb319ab72b97bd0e81abb5bb7bf76294cfa
faizulqa <167629736+faizulqa@users.noreply.github.com>
2024-04-20 18:11:15 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "faizulamir76@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2 //not allowed
+
+accountEmail = "faiz@faiz.com"
+accountPassword = "4333"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword])
