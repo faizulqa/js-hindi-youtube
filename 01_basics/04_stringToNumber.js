b4f5952f610464e593aa349d4888f696270cdd66
faizulqa <167629736+faizulqa@users.noreply.github.com>
2024-04-22 05:35:20 +0000

    comparision datatypes

---
+++ 01_basics/04_stringToNumber.js
@@ -0,0 +1,47 @@
+/*console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(9%12)
+console.log(5%2)*/
+
+
+
+
+
+/*let str1 = "hello"
+let str2 = " Faizul"
+
+console.log(str1 + str2)*/
+
+
+/*console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")*/
+
+/*let x = 3
+let y = x++
+console.log(x, y)
+
+let a = 3
+let b = ++a
+console.log(a, b)*/
+
+// console.log(+true)
+// console.log(+"")    
+
+// let gameCounter = 100
+// gameCounter++
+// // ++gameCounter
+// console.log(gameCounter)
+
+
+
+// console.log("2" > 1);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
