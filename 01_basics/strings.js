573144fd04c6a4d99d89921957238e20b904ebd3
faizulqa <167629736+faizulqa@users.noreply.github.com>
2024-04-22 09:22:59 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,17 @@
+
+
+
+
+const gameName = new String('hitesh-hc-com')
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
