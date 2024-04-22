2112b0e71483bedb599a14dfb3d23e41b25ce9f2
faizulqa <167629736+faizulqa@users.noreply.github.com>
2024-04-22 07:50:26 +0000

    strings

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,3 +1,24 @@
-const bigIntValue = 123456789012345678901234567890n
+// const bigIntValue = 123456789012345678901234567890n
 
-console.log(typeof bigIntValue)
+// console.log(typeof bigIntValue)
+
+
+// let myYoutubname = "hiteshchoudharycode"
+// let anotherName = myYoutubname
+
+// anotherName = "chaiaurcode"
+
+// console.log(myYoutubname)
+// console.log(anotherName)
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "faizul@gmail.com"
+
+console.log(userTwo.email)
+console.log(userOne.email)
