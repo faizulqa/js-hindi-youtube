a4ff25dffde7fee2409c800d4aeb76f28ad03999
faizulqa <167629736+faizulqa@users.noreply.github.com>
2024-05-08 11:30:33 +0000

    functions

---
+++ 02_basics/04_object.js
@@ -0,0 +1,128 @@
+
+
+
+
+
+
+
+
+
+
+const tinderUser2 = {}           
+
+tinderUser2.id = "123abc"
+tinderUser2.name = "Sammy"
+tinderUser2.isLoggedIn = false
+
+console.log(tinderUser2.hasOwnProperty('isLoggedIn'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*console.log(tinderUser2)
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+            userfullname:{
+                firstname: "Faizul",
+                lastname: "Amir"
+            }
+    }
+}
+
+console.log(regularUser.fullname?.userfullname.lastname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+const obj3 = {...obj1, ...obj2}
+
+console.log(obj3);*/
---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
